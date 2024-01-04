549b82135ffe7408aa3f1943128afc7e90b5653f
yenkus <mryenkus@gmail.com>
2024-01-03 23:04:23 -0800

    first actual commit and it has errors

--- lib/screens/settings_page.dart
+++ lib/screens/settings_page.dart
@@ -11,208 +11,198 @@ class SettingsScreen extends StatelessWidget {
     ThemeData appTheme = AppStateContainer.of(context).theme;
 
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: appTheme.primaryColor,
-          title: const Text("Settings"),
-        ),
-        body: Container(
-          padding: const EdgeInsets.only(left: 10, right: 10, top: 15),
-          color: appTheme.primaryColor,
-          child: ListView(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "Theme",
-                  style: TextStyle(
-                    color: appTheme.colorScheme.secondary,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
+      appBar: AppBar(
+        backgroundColor: appTheme.primaryColor,
+        title: const Text("Settings"),
+      ),
+      body: Container(
+        padding: const EdgeInsets.only(left: 10, right: 10, top: 15),
+        color: appTheme.primaryColor,
+        child: ListView(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Theme",
+                style: TextStyle(
+                  color: appTheme.colorScheme.secondary,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
                 ),
               ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(8),
-                      topRight: Radius.circular(8)),
-                  color: AppStateContainer.of(context)
-                      .theme
-                      .colorScheme
-                      .secondary
-                      .withOpacity(0.1),
-                ),
-                padding: const EdgeInsets.only(left: 10, right: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      "Dark",
-                      style: TextStyle(color: appTheme.colorScheme.secondary),
-                    ),
-                    Radio<int>(
-                      value: Themes.DARK_THEME_CODE,
-                      groupValue: AppStateContainer.of(context).themeCode,
-                      onChanged: (value) {
-                        AppStateContainer.of(context).updateTheme(value!);
-                      },
-                      activeColor: appTheme.colorScheme.secondary,
-                    )
-                  ],
-                ),
-              ),
-              Divider(
-                color: appTheme.primaryColor,
-                height: 1,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.only(
-                      bottomLeft: Radius.circular(8),
-                      bottomRight: Radius.circular(8)),
-                  color: AppStateContainer.of(context)
-                      .theme
-                      .colorScheme
-                      .secondary
-                      .withOpacity(0.1),
-                ),
-                padding: const EdgeInsets.only(left: 10, right: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      "Light",
-                      style: TextStyle(color: appTheme.colorScheme.secondary),
-                    ),
-                    Radio<int>(
-                      value: Themes.LIGHT_THEME_CODE,
-                      groupValue: AppStateContainer.of(context).themeCode,
-                      onChanged: (value) {
-                        AppStateContainer.of(context).updateTheme(value!);
-                      },
-                      activeColor: appTheme.colorScheme.secondary,
-                    )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    top: 15, left: 8, right: 8, bottom: 8),
-                child: Text(
-                  "Unit",
-                  style: TextStyle(
-                    color: appTheme.colorScheme.secondary,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(8),
-                      topRight: Radius.circular(8)),
-                  color: AppStateContainer.of(context)
-                      .theme
-                      .colorScheme
-                      .secondary
-                      .withOpacity(0.1),
-                ),
-                padding: const EdgeInsets.only(left: 10, right: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      "Celsius",
-                      style: TextStyle(color: appTheme.colorScheme.secondary),
-                    ),
-                    Radio<int>(
-                      value: TemperatureUnit.celsius.index,
-                      groupValue:
-                          AppStateContainer.of(context).temperatureUnit.index,
-                      onChanged: (value) {
-                        AppStateContainer.of(context).updateTemperatureUnit(
-                            TemperatureUnit.values[value!]);
-                      },
-                      activeColor: appTheme.colorScheme.secondary,
-                    )
-                  ],
-                ),
-              ),
-              Divider(
-                color: appTheme.primaryColor,
-                height: 1,
-              ),
-              Container(
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(8), topRight: Radius.circular(8)),
                 color: AppStateContainer.of(context)
                     .theme
-                    .colorScheme
-                    .secondary
+                    .colorScheme.secondary
                     .withOpacity(0.1),
-                padding: const EdgeInsets.only(left: 10, right: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      "Fahrenheit",
-                      style: TextStyle(color: appTheme.colorScheme.secondary),
-                    ),
-                    Radio<int>(
-                      value: TemperatureUnit.fahrenheit.index,
-                      groupValue:
-                          AppStateContainer.of(context).temperatureUnit.index,
-                      onChanged: (value) {
-                        AppStateContainer.of(context).updateTemperatureUnit(
-                            TemperatureUnit.values[value!]);
-                      },
-                      activeColor: appTheme.colorScheme.secondary,
-                    )
-                  ],
+              ),
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Dark",
+                    style: TextStyle(color: appTheme.colorScheme.secondary),
+                  ),
+                  Radio(
+                    value: Themes.DARK_THEME_CODE,
+                    groupValue: AppStateContainer.of(context).themeCode,
+                    onChanged: (value) {
+                      AppStateContainer.of(context).updateTheme(value);
+                    },
+                    activeColor: appTheme.colorScheme.secondary,
+                  )
+                ],
+              ),
+            ),
+            Divider(
+              color: appTheme.primaryColor,
+              height: 1,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(8),
+                    bottomRight: Radius.circular(8)),
+                color: AppStateContainer.of(context)
+                    .theme
+                    .colorScheme.secondary
+                    .withOpacity(0.1),
+              ),
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Light",
+                    style: TextStyle(color: appTheme.colorScheme.secondary),
+                  ),
+                  Radio(
+                    value: Themes.LIGHT_THEME_CODE,
+                    groupValue: AppStateContainer.of(context).themeCode,
+                    onChanged: (value) {
+                      AppStateContainer.of(context).updateTheme(value);
+                    },
+                    activeColor: appTheme.colorScheme.secondary,
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.only(top: 15, left: 8, right: 8, bottom: 8),
+              child: Text(
+                "Unit",
+                style: TextStyle(
+                  color: appTheme.colorScheme.secondary,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
                 ),
               ),
-              Divider(
-                color: appTheme.primaryColor,
-                height: 1,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(8), topRight: Radius.circular(8)),
+                color: AppStateContainer.of(context)
+                    .theme
+                    .colorScheme.secondary
+                    .withOpacity(0.1),
               ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.only(
-                      bottomLeft: Radius.circular(8),
-                      bottomRight: Radius.circular(8)),
-                  color: AppStateContainer.of(context)
-                      .theme
-                      .colorScheme
-                      .secondary
-                      .withOpacity(0.1),
-                ),
-                padding: const EdgeInsets.only(left: 10, right: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      "Kelvin",
-                      style: TextStyle(color: appTheme.colorScheme.secondary),
-                    ),
-                    Radio(
-                      value: TemperatureUnit.kelvin.index,
-                      groupValue:
-                          AppStateContainer.of(context).temperatureUnit.index,
-                      onChanged: (value) {
-                        // int val =
-                        AppStateContainer.of(context).updateTemperatureUnit(
-                            TemperatureUnit
-                                .values[int.parse(value.toString())]);
-                      },
-                      activeColor: appTheme.colorScheme.secondary,
-                    )
-                  ],
-                ),
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Celsius",
+                    style: TextStyle(color: appTheme.colorScheme.secondary),
+                  ),
+                  Radio(
+                    value: TemperatureUnit.celsius.index,
+                    groupValue:
+                        AppStateContainer.of(context).temperatureUnit.index,
+                    onChanged: (value) {
+                      AppStateContainer.of(context)
+                          .updateTemperatureUnit(TemperatureUnit.values[value]);
+                    },
+                    activeColor: appTheme.colorScheme.secondary,
+                  )
+                ],
               ),
-            ],
-          ),
-        ));
-  }
-}
+            ),
+            Divider(
+              color: appTheme.primaryColor,
+              height: 1,
+            ),
+            Container(
+              color: AppStateContainer.of(context)
+                  .theme
+                  .colorScheme.secondary
+                  .withOpacity(0.1),
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Fahrenheit",
+                    style: TextStyle(color: appTheme.colorScheme.secondary),
+                  ),
+                  Radio(
+                    value: TemperatureUnit.fahrenheit.index,
+                    groupValue:
+                        AppStateContainer.of(context).temperatureUnit.index,
+                    onChanged: (value) {
+                      AppStateContainer.of(context)
+                          .updateTemperatureUnit(TemperatureUnit.values[value]);
+                    },
+                    activeColor: appTheme.colorScheme.secondary,
+                  )
+                ],
+              ),
+            ),
+            Divider(
+              color: appTheme.primaryColor,
+              height: 1,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(8),
+                    bottomRight: Radius.circular(8)),
+                color: AppStateContainer.of(context)
+                    .theme
+                    .colorScheme.secondary
+                    .withOpacity(0.1),
+              ),
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Kelvin",
+                    style: TextStyle(color: appTheme.colorScheme.secondary),
+                  ),
+                  Radio(
+                    value: TemperatureUnit.kelvin.index,
+                    groupValue:
+                        AppStateContainer.of(context).temperatureUnit.index,
+                    onChanged: (value) {
+                      AppStateContainer.of(context)
+                          .updateTemperatureUnit(TemperatureUnit.values[value]);
+                    },
+                    activeColor: appTheme.colorScheme.secondary,
+                  )
+                ],
+              ),
+            ),
+          ],
+   ),
+));
+
 
 // import 'package:flutter/material.dart';
 
@@ -390,6 +380,7 @@ class SettingsScreen extends StatelessWidget {
 //     );
 //   }
 // }
+
 
 // import 'package:flutter/material.dart';
 
